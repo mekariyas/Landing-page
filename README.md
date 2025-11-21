1b8f4e95c99431c8efec5ecb0035c8a549f735e8
Mekariyas <mekariyas05@gmail.com>
2025-11-21 18:37:35 +0300

    Configured static image path

--- README.md
+++ README.md
@@ -1,50 +1,16 @@
-# React + TypeScript + Vite
+# React + TypeScript + Vite + TailwindCss + framer motion Landing page
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a simple landing page created using react-typescript , tailwindcss and framer motion. It has  navbar, Hero, About us, Amenities, Testimonials and a footer components.
 
-Currently, two official plugins are available:
+## How to run this project on a localsetup
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+For testing it on a local setup follow these steps:
 
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
-
-- Configure the top-level `parserOptions` property like this:
-
-```js
-export default tseslint.config({
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
-})
+```
+ git init
+ git clone https://github.com/mekariyas/Landing-page
+ cd Landing-page
+ npm install
+ npm run dev
 ```
 
-- Replace `tseslint.configs.recommended` to `tseslint.configs.recommendedTypeChecked` or `tseslint.configs.strictTypeChecked`
-- Optionally add `...tseslint.configs.stylisticTypeChecked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and update the config:
-
-```js
-// eslint.config.js
-import react from 'eslint-plugin-react'
-
-export default tseslint.config({
-  // Set the react version
-  settings: { react: { version: '18.3' } },
-  plugins: {
-    // Add the react plugin
-    react,
-  },
-  rules: {
-    // other rules...
-    // Enable its recommended rules
-    ...react.configs.recommended.rules,
-    ...react.configs['jsx-runtime'].rules,
-  },
-})
-```
--- src/staticData/data.ts
+++ src/staticData/data.ts
@@ -3,32 +3,32 @@ export const Testimonials=[
     {
         "name": "Jessica S. Harris",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur.",
-        "image": "src/assets/person/Person-1.webp"
+        "image": "../assets/person/Person-1.webp"
     },
     {
         "name":"Arridano Roussel",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur",       
-        "image":"src/assets/person/Person-2.webp"
+        "image":"../assets/person/Person-2.webp"
     },
     {
         "name": "Fred S. Arbour",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur",
-        "image":"src/assets/person/Person-3.webp"
+        "image":"../assets/person/Person-3.webp"
     },
     {
         "name":"Sarah S. Richardson",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur",
-        "image": "src/assets/person/Person-4.webp"
+        "image": "../assets/person/Person-4.webp"
     },
     {
         "name": "Edward C. Lymon",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur",
-        "image": "src/assets/person/Person-5.webp"
+        "image": "../assets/person/Person-5.webp"
     },
     {
         "name": "Pauline M. White",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur",
-        "image": "src/assets/person/Person-6.webp"
+        "image": "../assets/person/Person-6.webp"
     }]
 
 
@@ -37,40 +37,40 @@ export const Amenities = [
     {
         "name":"Bar",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "src/assets/Amenities/bar.webp"
+        "image": "../assets/Amenities/bar.webp"
     },
     {
         "name":"Conference Hall",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "src/assets/Amenities/conference.webp"
+        "image": "../assets/Amenities/conference.webp"
     },
     {
         "name":"Golf",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "src/assets/Amenities/golf.webp"
+        "image": "../assets/Amenities/golf.webp"
     },
     {
         "name":"Gym",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "src/assets/Amenities/gym.webp"
+        "image": "../assets/Amenities/gym.webp"
     },
     {
         "name":"Pool",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "src/assets/Amenities/poolside.webp"
+        "image": "../assets/Amenities/poolside.webp"
     },
     {
         "name":"24 hours Reception",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "src/assets/Amenities/reception.webp"
+        "image": "../assets/Amenities/reception.webp"
     },
     {
         "name":"High End Restaurant",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "src/assets/Amenities/restaurant.webp"
+        "image": "../assets/Amenities/restaurant.webp"
     },
     {
         "name":"Spa",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "src/assets/Amenities/spa.webp"
+        "image": "../assets/Amenities/spa.webp"
     }]
