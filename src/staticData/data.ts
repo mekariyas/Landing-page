153e02de44a7dada0ad8b5619de8b5d40dc300e2
Mekariyas <mekariyas05@gmail.com>
2025-11-21 18:43:15 +0300

    Configured static files path

--- src/staticData/data.ts
+++ src/staticData/data.ts
@@ -3,32 +3,32 @@ export const Testimonials=[
     {
         "name": "Jessica S. Harris",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur.",
-        "image": "../assets/person/Person-1.webp"
+        "image": "src/assets/person/Person-1.webp"
     },
     {
         "name":"Arridano Roussel",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur",       
-        "image":"../assets/person/Person-2.webp"
+        "image":"src/assets/person/Person-2.webp"
     },
     {
         "name": "Fred S. Arbour",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur",
-        "image":"../assets/person/Person-3.webp"
+        "image": "src/assets/person/Person-3.webp"
     },
     {
         "name":"Sarah S. Richardson",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur",
-        "image": "../assets/person/Person-4.webp"
+        "image": "src/assets/person/Person-4.webp"
     },
     {
         "name": "Edward C. Lymon",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur",
-        "image": "../assets/person/Person-5.webp"
+        "image": "src/assets/person/Person-5.webp"
     },
     {
         "name": "Pauline M. White",
         "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur",
-        "image": "../assets/person/Person-6.webp"
+        "image": "src/assets/person/Person-6.webp"
     }]
 
 
@@ -37,40 +37,40 @@ export const Amenities = [
     {
         "name":"Bar",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "../assets/Amenities/bar.webp"
+        "image": "src/assets/Amenities/bar.webp"
     },
     {
         "name":"Conference Hall",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "../assets/Amenities/conference.webp"
+        "image": "src/assets/Amenities/conference.webp"
     },
     {
         "name":"Golf",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "../assets/Amenities/golf.webp"
+        "image": "src/assets/Amenities/golf.webp"
     },
     {
         "name":"Gym",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "../assets/Amenities/gym.webp"
+        "image": "src/assets/Amenities/gym.webp"
     },
     {
         "name":"Pool",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "../assets/Amenities/poolside.webp"
+        "image": "src/assets/Amenities/poolside.webp"
     },
     {
         "name":"24 hours Reception",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "../assets/Amenities/reception.webp"
+        "image": "src/assets/Amenities/reception.webp"
     },
     {
         "name":"High End Restaurant",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "../assets/Amenities/restaurant.webp"
+        "image": "src/assets/Amenities/restaurant.webp"
     },
     {
         "name":"Spa",
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis nulla deserunt inventore voluptate ut, ipsa quae corrupti delectus veniam molestiae, facilis doloremque aspernatur! Maxime corporis eaque voluptatibus laboriosam exercitationem",
-        "image": "../assets/Amenities/spa.webp"
+        "image": "src/assets/Amenities/spa.webp"
     }]
